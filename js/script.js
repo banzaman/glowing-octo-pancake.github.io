ea6d5be7776b585d09d34d91beda6b528d5ce860
MARK RUBANZA <markrubanza10@gmail.com>
2022-03-11 21:44:57 +0300

    Validate Email input and made changes to the contact form

--- js/script.js
+++ js/script.js
@@ -103,5 +103,35 @@ function toggle(menu){
   popupArr.forEach((element) => {
     popelement(element);
   });
-  
-  
+
+  // Form Validation
+
+// Create variable for form, email, error message.
+const form = document.querySelector('#form');
+const idEmail = form.email;
+const errorMessage = document.getElementById('invalidText');
+
+// Check for lowercase
+function isLower(str) {
+  return !/[A-Z]/.test(str) && /[a-z]/.test(str);
+}
+
+// Create error function
+function errMsg() {
+  errorMessage.innerText = 'email has to be in lower case';
+}
+
+// Check for validation
+form.addEventListener('change', (e) => {
+  const validEmail = isLower(idEmail.value);
+  if (!validEmail) {
+    errMsg(e.target.name);
+  } else {
+    errorMessage.innerText = '';
+  }
+});
+// Prevent submit default
+form.addEventListener('submit', (e) => {
+  if (!isLower(idEmail
+.value)) { e.preventDefault(); }
+});
